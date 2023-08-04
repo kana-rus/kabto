3730561dbc7dd55eb4d00c4ab7c4e27f81b1554e
kana-rus <kanarus786@gmail.com>
2023-08-04 23:33:50 +0900

    Add: impl HTML for tuples

--- src/html/mod.rs
+++ src/html/mod.rs
@@ -1,14 +1,53 @@
 mod tag;
 
+use crate::{Cows, IntoCows};
+
 
 pub trait HTML {
-    fn render(self) -> crate::Cows;
+    fn render(self) -> Cows;
 }
 
-impl<IC: crate::IntoCows> HTML for IC {
-    fn render(self) -> crate::Cows {
-        self.into_cows()
+
+const _: () = {
+    impl HTML for () {
+        fn render(self) -> Cows {
+            Cows::Borrowed("")
+        }
     }
-}
+    impl<IC: IntoCows> HTML for IC {
+        fn render(self) -> Cows {
+            self.into_cows()
+        }
+    }
 
-// TODO: impl for (), (IC1,), (IC1,IC2), ...
+    macro_rules! impl_for_tuple {
+        ($( $name:ident ),+ $(,)?) => {
+            #[allow(non_snake_case)]
+            impl<$( $name:IntoCows, )+> HTML for ( $( $name, )+ ) {
+                fn render(self) -> Cows {
+                    let ( $( $name, )+ ) = self;
+                    Cows::Owned([$(
+                        $name.into_cows(),
+                    )+].concat())
+                }
+            }
+        };
+    } const _: () = {
+        impl_for_tuple!(IC1);
+        impl_for_tuple!(IC1, IC2);
+        impl_for_tuple!(IC1, IC2, IC3);
+        impl_for_tuple!(IC1, IC2, IC3, IC4);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9, IC10);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9, IC10, IC11);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9, IC10, IC11, IC12);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9, IC10, IC11, IC12, IC13);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9, IC10, IC11, IC12, IC13, IC14);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9, IC10, IC11, IC12, IC13, IC14, IC15);
+        impl_for_tuple!(IC1, IC2, IC3, IC4, IC5, IC6, IC7, IC8, IC9, IC10, IC11, IC12, IC13, IC14, IC15, IC16);
+    };
+};
