ba4b7c9a4f11dabf719ceae7bb42b3f4e7b7cef8
kanarus <kanarus786@gmail.com>
2023-08-14 01:20:05 +0900

    Add: `Node::None`, `tag::{body, header, p, span, h2, h3, h4}` (#2)

--- src/component/tag.rs
+++ src/component/tag.rs
@@ -1,3 +1,5 @@
+use std::marker::Tuple;
+
 use crate::{dom::{self, Node, Element, Tag}, library::IntoCows};
 use super::{IntoNode, NodeCollection};
 
@@ -37,7 +39,7 @@ impl IntoNode for a {
         dom::a::new()
             .rel(rel)
     }
-} impl<Children: NodeCollection> FnOnce<Children> for a {
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for a {
     type Output = Node;
     extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
         Node::Element(Element {
@@ -46,6 +48,63 @@ impl IntoNode for a {
         })
     }
 }
+
+pub struct p;
+impl IntoNode for p {
+    fn into_node(self) -> Node {
+        dom::p::new().into_node()
+    }
+} impl p {
+    pub fn class(self, class: impl IntoCows) -> dom::p {
+        dom::p::new()
+            .class(class)
+    }
+    pub fn id(self, id: impl IntoCows) -> dom::p {
+        dom::p::new()
+            .id(id)
+    }
+    pub fn style(self, style: impl IntoCows) -> dom::p {
+        dom::p::new()
+            .style(style)
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for p {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::p(dom::p::new()),
+            children: children.collect(),
+        })
+    }
+}
+
+pub struct span;
+impl IntoNode for span {
+    fn into_node(self) -> Node {
+        dom::span::new().into_node()
+    }
+} impl span {
+    pub fn class(self, class: impl IntoCows) -> dom::span {
+        dom::span::new()
+            .class(class)
+    }
+    pub fn id(self, id: impl IntoCows) -> dom::span {
+        dom::span::new()
+            .id(id)
+    }
+    pub fn style(self, style: impl IntoCows) -> dom::span {
+        dom::span::new()
+            .style(style)
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for span {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::span(dom::span::new()),
+            children: children.collect(),
+        })
+    }
+}
+
 
 pub struct div;
 impl IntoNode for div {
@@ -65,11 +124,67 @@ impl IntoNode for div {
         dom::div::new()
             .style(style)
     }
-} impl<Children: NodeCollection> FnOnce<Children> for div {
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for div {
     type Output = Node;
     extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
         Node::Element(Element {
             tag: Tag::div(dom::div::new()),
+            children: children.collect(),
+        })
+    }
+}
+
+pub struct body;
+impl IntoNode for body {
+    fn into_node(self) -> Node {
+        dom::body::new().into_node()
+    }
+} impl body {
+    pub fn class(self, class: impl IntoCows) -> dom::body {
+        dom::body::new()
+            .class(class)
+    }
+    pub fn id(self, id: impl IntoCows) -> dom::body {
+        dom::body::new()
+            .id(id)
+    }
+    pub fn style(self, style: impl IntoCows) -> dom::body {
+        dom::body::new()
+            .style(style)
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for body {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::body(dom::body::new()),
+            children: children.collect(),
+        })
+    }
+}
+
+pub struct header;
+impl IntoNode for header {
+    fn into_node(self) -> Node {
+        dom::header::new().into_node()
+    }
+} impl header {
+    pub fn class(self, class: impl IntoCows) -> dom::header {
+        dom::header::new()
+            .class(class)
+    }
+    pub fn id(self, id: impl IntoCows) -> dom::header {
+        dom::header::new()
+            .id(id)
+    }
+    pub fn style(self, style: impl IntoCows) -> dom::header {
+        dom::header::new()
+            .style(style)
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for header {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::header(dom::header::new()),
             children: children.collect(),
         })
     }
@@ -93,11 +208,95 @@ impl IntoNode for h1 {
         dom::h1::new()
             .style(style)
     }
-} impl<Children: NodeCollection> FnOnce<Children> for h1 {
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h1 {
     type Output = Node;
     extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
         Node::Element(Element {
             tag: Tag::h1(dom::h1::new()),
+            children: children.collect(),
+        })
+    }
+}
+
+pub struct h2;
+impl IntoNode for h2 {
+    fn into_node(self) -> Node {
+        dom::h2::new().into_node()
+    }
+} impl h2 {
+    pub fn class(self, class: impl IntoCows) -> dom::h2 {
+        dom::h2::new()
+            .class(class)
+    }
+    pub fn id(self, id: impl IntoCows) -> dom::h2 {
+        dom::h2::new()
+            .id(id)
+    }
+    pub fn style(self, style: impl IntoCows) -> dom::h2 {
+        dom::h2::new()
+            .style(style)
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h2 {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::h2(dom::h2::new()),
+            children: children.collect(),
+        })
+    }
+}
+
+pub struct h3;
+impl IntoNode for h3 {
+    fn into_node(self) -> Node {
+        dom::h3::new().into_node()
+    }
+} impl h3 {
+    pub fn class(self, class: impl IntoCows) -> dom::h3 {
+        dom::h3::new()
+            .class(class)
+    }
+    pub fn id(self, id: impl IntoCows) -> dom::h3 {
+        dom::h3::new()
+            .id(id)
+    }
+    pub fn style(self, style: impl IntoCows) -> dom::h3 {
+        dom::h3::new()
+            .style(style)
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h3 {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::h3(dom::h3::new()),
+            children: children.collect(),
+        })
+    }
+}
+
+pub struct h4;
+impl IntoNode for h4 {
+    fn into_node(self) -> Node {
+        dom::h4::new().into_node()
+    }
+} impl h4 {
+    pub fn class(self, class: impl IntoCows) -> dom::h4 {
+        dom::h4::new()
+            .class(class)
+    }
+    pub fn id(self, id: impl IntoCows) -> dom::h4 {
+        dom::h4::new()
+            .id(id)
+    }
+    pub fn style(self, style: impl IntoCows) -> dom::h4 {
+        dom::h4::new()
+            .style(style)
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h4 {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::h4(dom::h4::new()),
             children: children.collect(),
         })
     }
--- src/dom/tag.rs
+++ src/dom/tag.rs
@@ -1,11 +1,21 @@
-use crate::{library::{Cows, IntoCows}, component::{NodeCollection}};
+use std::marker::Tuple;
+
+use crate::{library::{Cows, IntoCows}, component::{NodeCollection, IntoNode}};
 use super::{components::{AnkerTarget, AnkerRel}, Node, Element};
 
 
-pub enum Tag {
+pub(crate) enum Tag {
     a(a),
+    p(p),
+    span(span),
+
     div(div),
+    body(body),
+    header(header),
     h1(h1),
+    h2(h2),
+    h3(h3),
+    h4(h4),
 } impl Tag {
     pub(crate) fn render_with_children(self, children: Vec<Node>, buf: &mut String) {
         match self {
@@ -14,15 +24,51 @@ pub enum Tag {
                 for c in children {c.render_to(buf)}
                 "</a>".render_to(buf)
             }
+            Self::p(p) => {
+                p.render_opening_to(buf);
+                for c in children {c.render_to(buf)}
+                "</p>".render_to(buf)
+            }
+            Self::span(span) => {
+                span.render_opening_to(buf);
+                for c in children {c.render_to(buf)}
+                "</span>".render_to(buf)
+            }
+
             Self::div(div) => {
                 div.render_opening_to(buf);
                 for c in children {c.render_to(buf)}
                 "</div>".render_to(buf)
             }
+            Self::body(body) => {
+                body.render_opening_to(buf);
+                for c in children {c.render_to(buf)}
+                "</body>".render_to(buf)
+            }
+            Self::header(header) => {
+                header.render_opening_to(buf);
+                for c in children {c.render_to(buf)}
+                "</header>".render_to(buf)
+            }
             Self::h1(h1) => {
                 h1.render_opening_to(buf);
                 for c in children {c.render_to(buf)}
                 "</h1>".render_to(buf)
+            }
+            Self::h2(h2) => {
+                h2.render_opening_to(buf);
+                for c in children {c.render_to(buf)}
+                "</h2>".render_to(buf)
+            }
+            Self::h3(h3) => {
+                h3.render_opening_to(buf);
+                for c in children {c.render_to(buf)}
+                "</h3>".render_to(buf)
+            }
+            Self::h4(h4) => {
+                h4.render_opening_to(buf);
+                for c in children {c.render_to(buf)}
+                "</h4>".render_to(buf)
             }
         }
     }
@@ -137,7 +183,14 @@ pub struct a {
 
         buf.push('>')
     }
-} impl<Children: NodeCollection> FnOnce<Children> for a {
+} impl IntoNode for a {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::a(self),
+            children: vec![],
+        })
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for a {
     type Output = Node;
     extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
         Node::Element(Element {
@@ -146,6 +199,92 @@ pub struct a {
         })
     }
 }
+
+pub struct p {
+    pub(crate) __base: BaseAttributes,
+} impl p {
+    pub fn class(mut self, class: impl IntoCows) -> Self {
+        self.__base.class.replace(class.into_cows());
+        self
+    }
+    pub fn id(mut self, id: impl IntoCows) -> Self {
+        self.__base.id.replace(id.into_cows());
+        self
+    }
+    pub fn style(mut self, style: impl IntoCows) -> Self {
+        self.__base.style.replace(style.into_cows());
+        self
+    }
+} impl p {
+    pub(crate) fn new() -> Self {
+        Self { __base: BaseAttributes::new() }
+    }
+    fn render_opening_to(self, buf: &mut String) {
+        let Self { __base } = self;
+        "<p".render_to(buf);
+        __base.render_to(buf);
+        buf.push('>')
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for p {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::p(self),
+            children: children.collect(),
+        })
+    }
+} impl IntoNode for p {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::p(self),
+            children: vec![],
+        })
+    }
+}
+
+pub struct span {
+    pub(crate) __base: BaseAttributes,
+} impl span {
+    pub fn class(mut self, class: impl IntoCows) -> Self {
+        self.__base.class.replace(class.into_cows());
+        self
+    }
+    pub fn id(mut self, id: impl IntoCows) -> Self {
+        self.__base.id.replace(id.into_cows());
+        self
+    }
+    pub fn style(mut self, style: impl IntoCows) -> Self {
+        self.__base.style.replace(style.into_cows());
+        self
+    }
+} impl span {
+    pub(crate) fn new() -> Self {
+        Self { __base: BaseAttributes::new() }
+    }
+    fn render_opening_to(self, buf: &mut String) {
+        let Self { __base } = self;
+        "<span".render_to(buf);
+        __base.render_to(buf);
+        buf.push('>')
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for span {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::span(self),
+            children: children.collect(),
+        })
+    }
+} impl IntoNode for span {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::span(self),
+            children: vec![],
+        })
+    }
+}
+
+
 
 pub struct div {
     pub(crate) __base: BaseAttributes,
@@ -172,12 +311,103 @@ pub struct div {
         __base.render_to(buf);
         buf.push('>')
     }
-} impl<Children: NodeCollection> FnOnce<Children> for div {
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for div {
     type Output = Node;
     extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
         Node::Element(Element {
             tag: Tag::div(self),
             children: children.collect(),
+        })
+    }
+} impl IntoNode for div {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::div(self),
+            children: vec![],
+        })
+    }
+}
+
+pub struct header {
+    pub(crate) __base: BaseAttributes,
+} impl header {
+    pub fn class(mut self, class: impl IntoCows) -> Self {
+        self.__base.class.replace(class.into_cows());
+        self
+    }
+    pub fn id(mut self, id: impl IntoCows) -> Self {
+        self.__base.id.replace(id.into_cows());
+        self
+    }
+    pub fn style(mut self, style: impl IntoCows) -> Self {
+        self.__base.style.replace(style.into_cows());
+        self
+    }
+} impl header {
+    pub(crate) fn new() -> Self {
+        Self { __base: BaseAttributes::new() }
+    }
+    fn render_opening_to(self, buf: &mut String) {
+        let Self { __base } = self;
+        "<header".render_to(buf);
+        __base.render_to(buf);
+        buf.push('>')
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for header {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::header(self),
+            children: children.collect(),
+        })
+    }
+} impl IntoNode for header {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::header(self),
+            children: vec![],
+        })
+    }
+}
+
+pub struct body {
+    pub(crate) __base: BaseAttributes,
+} impl body {
+    pub fn class(mut self, class: impl IntoCows) -> Self {
+        self.__base.class.replace(class.into_cows());
+        self
+    }
+    pub fn id(mut self, id: impl IntoCows) -> Self {
+        self.__base.id.replace(id.into_cows());
+        self
+    }
+    pub fn style(mut self, style: impl IntoCows) -> Self {
+        self.__base.style.replace(style.into_cows());
+        self
+    }
+} impl body {
+    pub(crate) fn new() -> Self {
+        Self { __base: BaseAttributes::new() }
+    }
+    fn render_opening_to(self, buf: &mut String) {
+        let Self { __base } = self;
+        "<body".render_to(buf);
+        __base.render_to(buf);
+        buf.push('>')
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for body {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::body(self),
+            children: children.collect(),
+        })
+    }
+} impl IntoNode for body {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::body(self),
+            children: vec![],
         })
     }
 }
@@ -207,12 +437,145 @@ pub struct h1 {
         __base.render_to(buf);
         buf.push('>')
     }
-} impl<Children: NodeCollection> FnOnce<Children> for h1 {
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h1 {
     type Output = Node;
     extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
         Node::Element(Element {
             tag: Tag::h1(self),
             children: children.collect(),
+        })
+    }
+} impl IntoNode for h1 {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::h1(self),
+            children: vec![],
+        })
+    }
+}
+
+pub struct h2 {
+    pub(crate) __base: BaseAttributes,
+} impl h2 {
+    pub fn class(mut self, class: impl IntoCows) -> Self {
+        self.__base.class.replace(class.into_cows());
+        self
+    }
+    pub fn id(mut self, id: impl IntoCows) -> Self {
+        self.__base.id.replace(id.into_cows());
+        self
+    }
+    pub fn style(mut self, style: impl IntoCows) -> Self {
+        self.__base.style.replace(style.into_cows());
+        self
+    }
+} impl h2 {
+    pub(crate) fn new() -> Self {
+        Self { __base: BaseAttributes::new() }
+    }
+    fn render_opening_to(self, buf: &mut String) {
+        let Self { __base } = self;
+        "<h2".render_to(buf);
+        __base.render_to(buf);
+        buf.push('>')
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h2 {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::h2(self),
+            children: children.collect(),
+        })
+    }
+} impl IntoNode for h2 {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::h2(self),
+            children: vec![],
+        })
+    }
+}
+
+pub struct h3 {
+    pub(crate) __base: BaseAttributes,
+} impl h3 {
+    pub fn class(mut self, class: impl IntoCows) -> Self {
+        self.__base.class.replace(class.into_cows());
+        self
+    }
+    pub fn id(mut self, id: impl IntoCows) -> Self {
+        self.__base.id.replace(id.into_cows());
+        self
+    }
+    pub fn style(mut self, style: impl IntoCows) -> Self {
+        self.__base.style.replace(style.into_cows());
+        self
+    }
+} impl h3 {
+    pub(crate) fn new() -> Self {
+        Self { __base: BaseAttributes::new() }
+    }
+    fn render_opening_to(self, buf: &mut String) {
+        let Self { __base } = self;
+        "<h3".render_to(buf);
+        __base.render_to(buf);
+        buf.push('>')
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h3 {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::h3(self),
+            children: children.collect(),
+        })
+    }
+} impl IntoNode for h3 {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::h3(self),
+            children: vec![],
+        })
+    }
+}
+
+pub struct h4 {
+    pub(crate) __base: BaseAttributes,
+} impl h4 {
+    pub fn class(mut self, class: impl IntoCows) -> Self {
+        self.__base.class.replace(class.into_cows());
+        self
+    }
+    pub fn id(mut self, id: impl IntoCows) -> Self {
+        self.__base.id.replace(id.into_cows());
+        self
+    }
+    pub fn style(mut self, style: impl IntoCows) -> Self {
+        self.__base.style.replace(style.into_cows());
+        self
+    }
+} impl h4 {
+    pub(crate) fn new() -> Self {
+        Self { __base: BaseAttributes::new() }
+    }
+    fn render_opening_to(self, buf: &mut String) {
+        let Self { __base } = self;
+        "<h4".render_to(buf);
+        __base.render_to(buf);
+        buf.push('>')
+    }
+} impl<Children: NodeCollection + Tuple> FnOnce<Children> for h4 {
+    type Output = Node;
+    extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
+        Node::Element(Element {
+            tag: Tag::h4(self),
+            children: children.collect(),
+        })
+    }
+} impl IntoNode for h4 {
+    fn into_node(self) -> Node {
+        Node::Element(Element {
+            tag: Tag::h4(self),
+            children: vec![],
         })
     }
 }
